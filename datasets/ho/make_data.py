a49e47e25074d788a0b33019d2c106d8beb511fb
Ryan Jarvis <ryan@technoloft.com>
2022-02-12 09:07:19 -0800

    Cleanup ho/make_data.py.   Modernized code.  Requires python3.7+.

--- datasets/ho/make_data.py
+++ datasets/ho/make_data.py
@@ -1,19 +1,29 @@
-import os
-import numpy as np
-import trimesh
-from PIL import Image
-import pickle
+from pathlib import Path
 
+import numpy as np
+
+# ========================================= #
+# Set Path here
+# EXAMPLE:
+#   ROOT = '/path/to/HO3D_v2'
+
+ROOT = None
+# ========================================= #
+
+
+if ROOT is None:
+    raise ValueError("Set the ROOT var to the path to the Hand-Object 3D Pose Annotation datset"
+                     "Data set can be found here: https://www.tugraz.at/index.php?id=40231 \n"
+                     "NOTE:  This application uses the V2 data.")
 
 # Change this path
-root = '/path/to/HO3D_v2'
-evaluation = os.path.join(root, 'evaluation')
-train = os.path.join(root, 'train')
+evaluation_root_path = Path(ROOT, 'evaluation')
+train_root_path = Path(ROOT, 'train')
+
 # Load object mesh
 reorder_idx = np.array([0, 13, 14, 15, 16, 1, 2, 3, 17, 4, 5, 6, 18, 10, 11, 12, 19, 7, 8, 9, 20])
 
 coordChangeMat = np.array([[1., 0., 0.], [0, -1., 0.], [0., 0., -1.]], dtype=np.float32)
-
 
 images_train = []
 points2d_train = []
@@ -24,31 +34,31 @@ points2d_val = []
 points3d_val = []
 
 # Train
-for subject in os.listdir(os.path.join(train)):
-    s_path = os.path.join(train, subject)
-    rgb = os.path.join(s_path, 'rgb')
-    meta = os.path.join(s_path, 'meta')
-    for rgb_file in os.listdir(rgb):
-        file_number = rgb_file.split('.')[0]
-        meta_file = os.path.join(meta, file_number+'.pkl')
-        img_path = os.path.join(rgb, rgb_file)        
+for subject in sorted(train_root_path.iterdir()):
+    rgb = Path(subject, 'rgb')
+    meta = Path(subject, 'meta')
+    for rgb_file in sorted(rgb.iterdir()):
+
+        meta_file = Path(meta, f'{rgb_file.stem}.pkl')
         data = np.load(meta_file, allow_pickle=True)
+
         cam_intr = data['camMat']
         hand3d = data['handJoints3D'][reorder_idx]
+
         obj_corners = data['objCorners3D']
         hand_object3d = np.concatenate([hand3d, obj_corners])
-        hand_object3d = hand_object3d.dot(coordChangeMat.T)        
+        hand_object3d = hand_object3d.dot(coordChangeMat.T)
         hand_object_proj = cam_intr.dot(hand_object3d.transpose()).transpose()
         hand_object2d = (hand_object_proj / hand_object_proj[:, 2:])[:, :2]
+
         if subject == 'MC6':
-            images_val.append(img_path)
+            images_val.append(rgb_file)
             points3d_val.append(hand_object3d)
             points2d_val.append(hand_object2d)
         else:
-            images_train.append(img_path)
+            images_train.append(rgb_file)
             points3d_train.append(hand_object3d)
             points2d_train.append(hand_object2d)
-
 
 images_train = np.array(images_train)
 points2d_train = np.array(points2d_train)
@@ -66,20 +76,18 @@ np.save('images-train.npy', images_train)
 np.save('points2d-train.npy', points2d_train)
 np.save('points3d-train.npy', points3d_train)
 
-
 images_test = []
 points2d_test = []
 points3d_test = []
+
 # Evaluation
-for subject in os.listdir(os.path.join(evaluation)):
-    s_path = os.path.join(evaluation, subject)
-    rgb = os.path.join(s_path, 'rgb')
-    meta = os.path.join(s_path, 'meta')
-    for rgb_file in os.listdir(rgb):
-        file_number = rgb_file.split('.')[0]
-        meta_file = os.path.join(meta, file_number+'.pkl')
-        img_path = os.path.join(rgb, rgb_file)
+for subject in sorted(evaluation_root_path.iterdir()):
+    rgb = Path(subject, 'rgb')
+    meta = Path(subject, 'meta')
+    for rgb_file in sorted(rgb.iterdir()):
+        meta_file = Path(meta, f'{rgb_file.stem}.pkl')
         data = np.load(meta_file, allow_pickle=True)
+
         cam_intr = data['camMat']
         hand3d = np.repeat(np.expand_dims(data['handJoints3D'], 0), 21, 0)
         hand3d = hand3d[reorder_idx]
@@ -88,10 +96,10 @@ for subject in os.listdir(os.path.join(evaluation)):
         hand_object3d = hand_object3d.dot(coordChangeMat.T)
         hand_object_proj = cam_intr.dot(hand_object3d.transpose()).transpose()
         hand_object2d = (hand_object_proj / hand_object_proj[:, 2:])[:, :2]
-        images_test.append(img_path)
+
+        images_test.append(rgb_file)
         points3d_test.append(hand_object3d)
         points2d_test.append(hand_object2d)
-
 
 images_test = np.array(images_test)
 points2d_test = np.array(points2d_test)
@@ -100,4 +108,3 @@ points3d_test = np.array(points3d_test)
 np.save('images-test.npy', images_test)
 np.save('points2d-test.npy', points2d_test)
 np.save('points3d-test.npy', points3d_test)
-
